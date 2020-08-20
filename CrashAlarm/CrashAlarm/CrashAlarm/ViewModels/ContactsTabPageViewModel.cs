08b418fe24d6fbe7567ee2a3c8717b67d98048a4
jhala <jhala@4ps.nl>
2020-08-20 19:56:39 +0200

    Contact page and ViewModel

--- CrashAlarm/CrashAlarm/CrashAlarm/ViewModels/ContactsTabPageViewModel.cs
+++ CrashAlarm/CrashAlarm/CrashAlarm/ViewModels/ContactsTabPageViewModel.cs
@@ -2,15 +2,46 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
+using CrashAlarm.Models;
+using CrashAlarm.ViewModels;
+using Xamarin.Forms;
 
 namespace CrashAlarm.ViewModels
 {
     public class ContactsTabPageViewModel : BindableBase
     {
+        readonly DbRepository _db;
         public ContactsTabPageViewModel()
         {
+            _db = new DbRepository();
+            List<Contact> result = _db.GetAllContactsAsync().Result;
+            foreach (var item in result)
+            {
+                Contacts.Add(item);   
+            }
 
         }
+
+        public ObservableCollection<Contact> Contacts { get; } = new ObservableCollection<Contact>();
+
+        public ICommand DeleteCommand => new Command<Contact>((Contact item) =>
+        { // nefunkcni
+            System.Console.WriteLine($"Delete command was called on: {item.ContactName}");
+        });
+
+        public Command<Contact> RemoveCommand
+        {
+            get
+            {
+                return new Command<Contact>((Contact) => {
+                    Contacts.Remove(Contact);
+                });
+            }
+        }
+
+        //App.Current.MainPage.DisplayAlert("Alert", "There is no item in the list", "OK");
     }
 }
--- CrashAlarm/CrashAlarm/CrashAlarm/Views/ContactsTabPage.xaml.cs
+++ CrashAlarm/CrashAlarm/CrashAlarm/Views/ContactsTabPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CrashAlarm.Models;
+using CrashAlarm.ViewModels;
 using Xamarin.Forms;
 
 namespace CrashAlarm.Views
@@ -9,11 +10,13 @@ namespace CrashAlarm.Views
         public ContactsTabPage()
         {
             InitializeComponent();
+            BindingContext = new ContactsTabPageViewModel();
         }
         protected override async void OnAppearing()
         {
             base.OnAppearing();
             listView.ItemsSource = await App.DbRepository.GetAllContactsAsync();
+            //listViewFirst.ItemsSource = await App.DbRepository.GetAllContactsAsync();
         }
 
         async void OnButtonClicked(object sender, EventArgs e)
@@ -36,6 +39,20 @@ namespace CrashAlarm.Views
                 NameEntry.Text = NumberEntry.Text = string.Empty;
                 listView.ItemsSource = await App.DbRepository.GetAllContactsAsync();
             }
+        }
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var menuItem = ((MenuItem)sender);
+            //DisplayAlert("Delete Context Action", menuItem.CommandParameter + " delete context action", "OK");
+            var result = App.DbRepository.DeleteContactAsync((Contact)menuItem.BindingContext);
+            
+        }
+        public void Remove_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var contactItem = button.BindingContext as Contact;
+            var vm = BindingContext as ContactsTabPageViewModel;
+            vm.RemoveCommand.Execute(contactItem);
         }
     }
 
