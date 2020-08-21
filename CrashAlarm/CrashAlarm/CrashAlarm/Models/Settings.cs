ea5af3acc8280143ef6db7fd248175302ae93e98
jhala <jhala@4ps.nl>
2020-08-21 08:36:54 +0200

    Change structure of Settings

--- CrashAlarm/CrashAlarm/CrashAlarm/Models/Settings.cs
+++ CrashAlarm/CrashAlarm/CrashAlarm/Models/Settings.cs
@@ -7,14 +7,76 @@ namespace CrashAlarm.Models
 {
     public class Settings
     {
-        [PrimaryKey, AutoIncrement]
+        private bool _gsmNotificationToEmergency;
+        private bool _gsmNotificationToFriends;
+        private bool _gsmNotificationToFamily;
+        private string _helpMessage;
+        private DateTime _lastUpdate;
+
+       [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-        public DateTime LastUpdate { get; set; }
-        public string Units { get; set; }
-        public string Accuracy { get; set; }
-        public bool GSMNotificationToEmergency { get; set; }
-        public bool GSMNotificationToFriends { get; set; }
+        public DateTime LastUpdate 
+        {
+            get => _lastUpdate;
+            set
+            {
+                if (_lastUpdate != value)
+                {
+                    _lastUpdate = value;
+                    App.DbRepository.SaveSettingsAsync(this);
+                }
+            }
+        }
+        public string HelpMessage
+        {
+            get => _helpMessage;
+            set
+            {
+                if (_helpMessage != value)
+                {
+                    _helpMessage = value;
+                    App.DbRepository.SaveSettingsAsync(this);
+                }
+            }
+        }
+       
+        public bool GSMNotificationToEmergency
+        {
+            get => _gsmNotificationToEmergency;
+            set
+            { 
+                if (_gsmNotificationToEmergency != value)
+                {
+                    _gsmNotificationToEmergency = value;
+                    App.DbRepository.SaveSettingsAsync(this);
+                }
+            }
+        }
 
+        public bool GSMNotificationToFriends
+        {
+            get => _gsmNotificationToFriends;
+            set
+            {
+                if (_gsmNotificationToFriends != value)
+                {
+                    _gsmNotificationToFriends = value;
+                    App.DbRepository.SaveSettingsAsync(this);
+                }
+            }
+        }
 
+        public bool GSMNotificationToFamily
+        {
+            get => _gsmNotificationToFamily;
+            set
+            {
+                if (_gsmNotificationToFamily != value)
+                {
+                    _gsmNotificationToFamily = value;
+                    App.DbRepository.SaveSettingsAsync(this);
+                }
+            }
+        }
     }
 }
