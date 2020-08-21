c04d25ebc9c5c4ccfd532512ea0230ad22859174
jhala <jhala@4ps.nl>
2020-08-21 10:48:29 +0200

    MainPage + VM + codebehind

--- CrashAlarm/CrashAlarm/CrashAlarm/ViewModels/MainTabPageViewModel.cs
+++ CrashAlarm/CrashAlarm/CrashAlarm/ViewModels/MainTabPageViewModel.cs
@@ -3,14 +3,152 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using CrashAlarm.Models;
+using ImTools;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Color = System.Drawing.Color;
 
 namespace CrashAlarm.ViewModels
 {
     public class MainTabPageViewModel : BindableBase
     {
+        private Location _location;
+        private string _longitude;
+        private string _latitude;
+        private int _numberOfContacts;
+
         public MainTabPageViewModel()
         {
+            getLocationAsync();
+            SendSMSCommand = new Command(async () => await CallForHelp());
 
         }
+
+        public string Longitude
+        {
+            get => _longitude;
+            set
+            {
+                _longitude = value;
+                RaisePropertyChanged(nameof(Longitude));
+            }
+        }
+
+        public string Latitude
+        {
+            get => _latitude;
+            set
+            {
+                _latitude = value;
+                RaisePropertyChanged(nameof(Latitude));
+            }
+        }
+        public int NumberOfContacts
+        {
+            get => _numberOfContacts;
+            set
+            {
+                _numberOfContacts = value;
+                RaisePropertyChanged(nameof(NumberOfContacts));
+            }
+        }
+
+        private async void getLocationAsync()
+        {
+            try
+            {
+                _location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (_location != null)
+                {
+                    Longitude = _location.Longitude.ToString();
+                    Latitude = _location.Latitude.ToString();
+
+                    Console.WriteLine($"Latitude: {_location.Latitude}, Longitude: {_location.Longitude}, Altitude: {_location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+
+            }
+            catch (PermissionException pEx)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public ICommand SendSMSCommand { get; set; }
+
+        private async Task SendSms(string messageText, string[] recipients)
+        {
+            try
+            {
+                var message = new SmsMessage(messageText, recipients);
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+        private async Task CallForHelp()
+        {
+            if (_location != null)
+            {
+                var setting = await App.DbRepository.GetSettingsAsync();
+
+                List<Contact> contactList = new List<Contact>();
+                List<string> contactNumbers = new List<string>();
+
+
+                if (setting.GSMNotificationToFriends)
+                    contactList.AddRange(await App.DbRepository.GetFriendContactsAsync());
+
+                if (setting.GSMNotificationToFamily)
+                    contactList.AddRange(await App.DbRepository.GetFamilyContactsAsync());
+
+                if (setting.GSMNotificationToEmergency)
+                    contactList.AddRange(await App.DbRepository.GetEmergencyContactsAsync());
+
+                NumberOfContacts = contactList.Count;
+
+                contactNumbers.AddRange(contactList.Select(x => x.ContactNumber));
+
+                string messageToSend =
+                    setting.HelpMessage + $" My Location (Lon: {_location.Longitude}, Lat: {_location.Latitude}";
+
+
+                //await SendSms(messageToSend, contactNumbers.ToArray());
+
+                //UserDialogs.Instance.Toast("Text", TimeSpan.FromSeconds(3));
+                //UserDialogs.Instance.Toast(new ToastConfig("text") {BackgroundColor = Color.Pink});
+
+                var res = await UserDialogs.Instance.ConfirmAsync("zprava", "TItle", "ok", "nic");
+
+            }
+            // check location
+            // check settings
+            // check contact list
+            // send all messages
+
+            // show notification
+        }
+
     }
 }
--- CrashAlarm/CrashAlarm/CrashAlarm/Views/MainTabPage.xaml.cs
+++ CrashAlarm/CrashAlarm/CrashAlarm/Views/MainTabPage.xaml.cs
@@ -11,101 +11,24 @@ namespace CrashAlarm.Views
 {
     public partial class MainTabPage : ContentPage
     {
-        private Location location;
+
 
 
         public MainTabPage()
         {
             InitializeComponent();
-            getLocationAsync();
+
             
 
         }
 
         async void clickedOnHelpButton(object sender, EventArgs e)
         {
-            if (location != null)
-            {
-                var setting = await App.DbRepository.GetSettingsAsync();
-
-                List<Contact> contactList = new List<Contact>();
-                List<string> contactNumbers = new List<string>();
-
-                if (setting.GSMNotificationToFriends)
-                    contactList.AddRange(await App.DbRepository.GetFriendContactsAsync());
-
-                if (setting.GSMNotificationToEmergency)
-                    contactList.AddRange(await App.DbRepository.GetEmergencyContactsAsync());
-
-                var contactListCount = contactList.Count;
-
-                contactNumbers.AddRange(contactList.Select(x=>x.ContactNumber));
-
-                string messageToSend =
-                    setting.Units + $" My Location (Lon: {location.Longitude}, Lat: {location.Latitude}";
-
-
-                await SendSms(messageToSend, contactNumbers.ToArray());
-
-               
-
-            }
-            // check location
-            // check settings
-            // check contact list
-            // send all messages
-
-            // show notification
+            
         }
 
-        public async Task SendSms(string messageText, string[] recipients)
-        {
-            try
-            {
-                var message = new SmsMessage(messageText, recipients);
-                await Sms.ComposeAsync(message);
-            }
-            catch (FeatureNotSupportedException ex)
-            {
-                // Sms is not supported on this device.
-            }
-            catch (Exception ex)
-            {
-                // Other error has occurred.
-            }
-        }
+        
 
-        async void getLocationAsync()
-        {
-            try
-            {
-                //var accessStatus = GeolocationRequest;
-
-                location = await Geolocation.GetLastKnownLocationAsync();
-
-                if (location != null)
-                {
-                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
-                    latLabel.Text = location.Latitude.ToString();
-                    longLabel.Text = location.Longitude.ToString();
-                }
-            }
-            catch (FeatureNotSupportedException fnsEx)
-            {
-              
-            }
-            catch (FeatureNotEnabledException fneEx)
-            {
-               
-            }
-            catch (PermissionException pEx)
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                
-            }
-        }
+        
     }
 }
